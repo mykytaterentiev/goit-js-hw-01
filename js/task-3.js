000124117316fe1bb41e8bb757eefb6fe1dd25bc
mykytaterentiev <mykytaterentievua@gmail.com>
2023-11-11 19:01:12 +0100

    19:00

--- js/task-3.js
+++ js/task-3.js
@@ -6,3 +6,7 @@ const contentWidth = parseFloat(content);
 
   return totalWidth;
 };
+
+console.log(getElementWidth("50px", "8px", "4px")); 
+console.log(getElementWidth("60px", "12px", "8.5px")); 
+console.log(getElementWidth("200px", "0px", "0px")); 
